ac0c610e59e85ed94b6a29930c4a9cd667ab9cb9
beingrajput7233 <rajputashu7200@gmail.com>
2024-02-11 00:15:15 +0530

    Use of Path variable

--- spring-boot-rest-crud/src/main/java/com/jsr/demo/rest/StudentRestController.java
+++ spring-boot-rest-crud/src/main/java/com/jsr/demo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.jsr.demo.rest;
 
 import com.jsr.demo.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +14,25 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents(){
-        List<Student> l=new ArrayList<>();
+    private List<Student> l;
 
+    @PostConstruct
+    public void loadData(){
+//        it is done so that list is created only once for every run of main application not for every http request
+        l=new ArrayList<>();
         l.add(new Student("Ashutosh","Singh"));
         l.add(new Student("A","S"));
+    }
 
+    @GetMapping("/students")
+    public List<Student> getStudents(){
 //        spring will use Jackson in background
         return l;
+    }
+
+    // "/students/{studentId}" ---return a single student....by index of array list
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+        return l.get(studentId);
     }
 }
