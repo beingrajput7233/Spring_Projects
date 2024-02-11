d53aaba9d1e4c45e67a4070bbff16e3954e0450a
beingrajput7233 <rajputashu7200@gmail.com>
2024-02-11 14:40:33 +0530

    added Generic exception handler to handle any exception

--- spring-boot-rest-crud/src/main/java/com/jsr/demo/rest/StudentRestController.java
+++ spring-boot-rest-crud/src/main/java/com/jsr/demo/rest/StudentRestController.java
@@ -42,7 +42,7 @@ public class StudentRestController {
         return l.get(studentId);
     }
 
-    // Adding exception handler
+    // Adding exception handler to address StudentNotFound Exception
     @ExceptionHandler
     public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException excep){
         StudentErrorResponse error=new StudentErrorResponse();
@@ -54,5 +54,19 @@ public class StudentRestController {
         error.setTimeStamp(System.currentTimeMillis());
 
         return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+    }
+
+    // add another handler to handle any error
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
+        StudentErrorResponse error=new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+
+        error.setMessage(exc.getMessage());
+
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
     }
 }
